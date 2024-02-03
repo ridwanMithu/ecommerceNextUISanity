65a98feabe43ec04eb5913e1c9e63ec4dfb12d10
ridwanmithu <ridwan.mithu@outlook.com>
2024-02-04 00:00:38 +0600

    Next UI installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { NextUIProvider } from '@nextui-org/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-
+      <NextUIProvider>
+        <h1>If you are here then you have NextUI</h1>
+      </NextUIProvider>
         
     </>
   )
