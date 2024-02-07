89231c6d6ef5aeec14c70f665ca77892ef73236f
ridwanmithu <ridwan.mithu@outlook.com>
2024-02-07 22:32:11 +0600

    search functionality with auto update added. so if you start entering data now it will start to automatically show products with similar product name

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -6,15 +6,40 @@ import { client } from "../Config/SanityConfig";
 
 export default function AllProducts() {
   const [grocery, setGrocery]=useState([]);
-  
+  const[category,setCategory]=useState([])
+  //to get category list
+  const getCategory=async()=>{
+  let catquery= `*[_type=="category"]{categoryName, _id}`;
+  const categoryData= await client.fetch(catquery);
+  setCategory(categoryData);}
+
+    const  handleSearch = (evt) => {
+      // console.log(e.target.value); 
+      const searchEntry=evt.target.value; 
+      productSearch(searchEntry);
+    }
+
+    const productSearch=async(searchValue)=>{
+      
+      const searchQuery=`*[_type=="allproducts" && (productName match "${searchValue}*")]
+      {productName, productPrice, productAmount,productStock,
+        "imageUrl": productImage.asset->url,"category":category->categoryName}`;
+      const searchResult=await client.fetch(searchQuery);
+      setGrocery(searchResult);
+    }
+
+
+      
+  // to get grocery items
   const getGrocery=async ()=>{
     const groceryQuery=`*[_type=="allproducts"]{productName,productAmount,"imageUrl": productImage.asset->url,productStock, "type":category->categoryName,_id,productPrice }`
     const groceryData=await client.fetch(groceryQuery);
     setGrocery(groceryData)
    };
-   console.log(grocery);
    useEffect(()=>{
     getGrocery();
+    getCategory();
+    productSearch();
    },[]);
 
   return (
@@ -25,14 +50,12 @@ export default function AllProducts() {
           <link rel="canonical" href="/home" />
         </Helmet>
       <div>
-      <form className="flex justify-center"><Input type="text" label="Search your item" className="max-w-md " />
+      <form className="flex justify-center"><Input name="userSearchValue" type="text" label="Search your item" className="max-w-md " onChange={handleSearch} />
         <Select 
-        label="Select a Product" 
+        label="Select a Category" 
         className="max-w-md mx-10" 
       >
-      <SelectItem>Meat Products</SelectItem>
-      <SelectItem>Dairy</SelectItem>
-      <SelectItem>Fruits</SelectItem>
+       {category.map(item=> <SelectItem key={item._id} value={item.categoryName}>{item.categoryName}</SelectItem>)}
       </Select>
         </form>
       </div>
